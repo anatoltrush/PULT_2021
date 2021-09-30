34bbf9478198095e251963ab877d2d79ed102e68
Anatoly <a.trush@insy.dev>
2021-09-30 17:06:50 +0300

    small fixes

--- WrapRadio.h
+++ WrapRadio.h
@@ -13,8 +13,10 @@ class WrapRadio
     RF24* radio = NULL;
 
     uint8_t pipeNum = 0;
+
     // ACK bytes: 0 - quad volt, 1 - is max reached, 2 - num of motor
     uint8_t ack_msg[SIZE_OF_ACK] = {0};
+
     // left[1]throttle, left[0]yaw, right[4]pitch, right[3]roll
     uint8_t msg_data[SIZE_OF_DATA] = {0};
 
